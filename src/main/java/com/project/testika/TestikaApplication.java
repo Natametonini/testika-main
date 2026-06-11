94450297ffd2687ebc28b7307b979691367b581e
Victor Hugo <211873799+Victor140113@users.noreply.github.com>
2026-06-10 21:05:56 -0300

    feat: Initial Structure

---
+++ src/main/java/com/project/testika/TestikaApplication.java
@@ -0,0 +1,12 @@
+package com.project.testika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestikaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/project/testika/TestikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.testika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
